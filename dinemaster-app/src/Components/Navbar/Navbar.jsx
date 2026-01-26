b63c1d01ab4a8cf207c7c4590e3931db3fb61e91
codingharsha <harshavarthanoff@gmail.com>
2026-01-26 11:28:24 +0530

    Navbar Completed

--- dinemaster-app/src/Components/Navbar/Navbar.jsx
+++ dinemaster-app/src/Components/Navbar/Navbar.jsx
@@ -7,44 +7,49 @@ import { FaRegUser } from "react-icons/fa6";
 import { IoSearch } from "react-icons/io5";
 import { FaFire } from "react-icons/fa";
 import { RxHamburgerMenu } from "react-icons/rx";
+
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className= 'navbar'>
         <div className="navbar-container">
-            <div className="logo-section">
-                <FaFire className='logo-img'/>
-                <div className="logo-txt">
-                    <div className="logo-title">DineMaster</div>
-                    <div className="logo-slogan">AUTHENTIC FLAVOURS</div>    
-                </div> 
+          <div className="logo-container">
+             <div className="icon-wrapper">
+               <FaFire className='logo'/>
+             </div>
+             <div className="logo-txt-container">
+               <div className='logo-txt'>DineMaster</div>
+               <div className='logo-slogan'>AUTHENTIC FLAVOURS</div>
+             </div>
+          </div>
+
+          <div className="links">
+            <div >Deals</div>
+            <div >Happiness Cards</div>
+            <div>Restaurants</div>
+            <div>Takeaway</div>
+          </div>
+
+          <div className="right-section">
+            
+            <div className="icons">
+               <div className="icon-wrapper"><IoSearch className='search-icon'/></div>
+               <div className="icon-wrapper"><RiShoppingCart2Line className='cart-icon'/></div>
             </div>
 
-            <div className="mid-section">
-                <div className="links">
-                    <div className="link">Deals</div>
-                    <div className="link">Happiness Cards</div>
-                    <div className="link">Restaurants</div>
-                </div>
-            </div>
+            <button className='location-btn'>
+              <div className="icon-wrapper"><FaLocationArrow /></div>
+              <span className='location-txt'>Select Location</span>
+              <div className="icon-wrapper"><MdOutlineKeyboardArrowDown /></div>
+            </button>
 
-            <div className="right-section">
-                <div className="buttons">
-                    <IoSearch className='icon-wrapper'/>
-                    <RiShoppingCart2Line className='icon-wrapper'/>
-                </div>
-                <button className='location-button'>
-                    <FaLocationArrow className='btn-icon-wrapper'/>
-                    <div className="location-txt">Select Your Location</div>
-                    <MdOutlineKeyboardArrowDown className='btn-icon-wrapper'/>
-                </button>
-                <button className="profile-button">
-                    <FaRegUser className='btn-icon-wrapper'/>
-                    <div className="profile-txt">Log In</div>
-                </button>
-                <div className="sidebar-button">
-                    <RxHamburgerMenu className='btn-icon-wrapper'/>
-                </div>
-            </div>
+            <button className='profile'>
+              <div className="icon-wrapper"><FaRegUser /></div>
+              <div className='profile-txt'>Log In</div>
+            </button>
+
+            <div className="icon-wrapper"><RxHamburgerMenu /></div>
+          </div>
+
         </div>
     </div>
   )
